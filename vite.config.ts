b47969c6020358ab3ac6bc116d9629c60d29ac62
asperumalla <alphasovereign@gmail.com>
2025-10-24 21:21:12 -0400

    Rename project from budget-guard-app to app

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command }) => ({
-  base: command === 'build' ? '/budget-guard-app/' : '/',
+  base: command === 'build' ? '/app/' : '/',
   server: {
     host: "::",
     port: 8080,
